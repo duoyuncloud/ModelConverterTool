f8145b6cc3a9219e8bdfaed1615ebebebb0a8fe6
duoyuncloud <dy325@cam.ac.uk>
2025-07-21 18:11:22 +0800

    fix import issue

--- tools/llama.cpp/gguf-py/gguf/__init__.py
+++ tools/llama.cpp/gguf-py/gguf/__init__.py
@@ -12,10 +12,20 @@ from .constants import (
     RopeScalingType,
     PoolingType,
     MODEL_ARCH,
+    LlamaFileType,
+    MODEL_ARCH_NAMES,
+    MODEL_TENSOR,
+    MODEL_TENSORS,
+    TENSOR_NAMES,
+    GGUFType,
+    GGML_QUANT_VERSION,
 )
 from .quants import quant_shape_from_byte_shape, quant_shape_to_byte_shape
-from .utility import fill_templated_filename, naming_convention
+from .utility import fill_templated_filename, naming_convention, size_label
 from .metadata import Metadata
+from .lazy import LazyBase, LazyNumpyTensor
+from .tensor_mapping import get_tensor_name_map
+from .vocab import SpecialVocab
 
 __all__ = [
     "GGUFReader",
@@ -34,6 +44,18 @@ __all__ = [
     "quant_shape_to_byte_shape",
     "fill_templated_filename",
     "naming_convention",
+    "size_label",
     "Metadata",
     "MODEL_ARCH",
+    "LazyBase",
+    "LlamaFileType",
+    "MODEL_ARCH_NAMES",
+    "get_tensor_name_map",
+    "LazyNumpyTensor",
+    "MODEL_TENSOR",
+    "MODEL_TENSORS",
+    "TENSOR_NAMES",
+    "GGUFType",
+    "GGML_QUANT_VERSION",
+    "SpecialVocab",
 ]
